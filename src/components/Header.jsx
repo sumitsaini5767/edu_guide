e146d383542453798fc3a6ec6dc41162a70120e7
sumit <sumitsaini576@gmail.com>
2023-11-02 12:14:29 +0530

    .

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <div></div>
+        <div></div>
+        <div><Button>Logout</Button></div>    
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <form>
+      
+    </form>
+  )
+}
+
+export default LoginPage
