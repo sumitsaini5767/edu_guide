87a601e4af0f4c7ebc5bb212e33181f88ba880f9
Tanisha Gupta <tanishagupta27200@gmail.com>
2023-11-02 15:41:36 +0530

    .

--- src/pages/SignupPge.jsx
+++ src/pages/SignupPge.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const SignupPge = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center h-screen w-full'>
+        <Formik
+         initialValues={{ fullname: "", email: "", password: "" }}
+         validate={(values) => {
+           const errors = {};
+           if (!values.fullname) {
+             errors.fullname = "Required";
+           }
+
+           if (!values.email) {
+             errors.email = "Required";
+           } else if (
+             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+           ) {
+             errors.email = "Invalid email address";
+           }
+           if (!values.password) {
+             errors.password = "Required";
+           }
+           return errors;
+         }}
+       >
+        {({ isSubmitting }) => (
+           <Form className="flex flex-col w-2/5 h-screen bg-bgcolor-600 rounded-md p-12">
+             <p className='head'>Welcome To Eduor!</p>
+             <p className='para'>Sign Up To Continue</p>
+             <label className='spara'>Name</label>
+             <Field
+               type="text"
+               name="fullname"
+               placeholder="Enter your fullname"
+             />
+             <ErrorMessage name="fullname" component="div" />
+
+             <Field
+               type="email"
+               name="email"
+               placeholder="Enter email address"
+             />
+             <ErrorMessage name="email" component="div" />
+
+             <Field type="password" name="password" placeholder="Enter password"/>
+             <ErrorMessage name="password" component="div" />
+
+             <button type="submit" disabled={isSubmitting}>
+               Submit
+             </button>
+           </Form>
+         )}
+       </Formik>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        bgcolor:{
+          600:"#e9f7fa"
+        },
+        "paracolor":"#636363",
+      },
+      fontSize:{
+        head:"2.25rem",
+        subhead:"1.0625rem",
+        para:"1.06rem",
+        spara:"0.93rem"
+      },
+      fontWeight:{
+        head:"700",
+        subhead:"600",
+        para:"400",
+        spara:"400"
+      }
+    },
   },
   plugins: [],
 }
