d743cbd9a063285ae59f62bea17b5067a44a6488
Tanisha Gupta <tanishagupta27200@gmail.com>
2023-11-05 18:00:25 +0530

    .

---
+++ eduor/src/firebase_config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6UEyXaPGfWk_J5x1kT6r4yxOA7hvHJII",
+  authDomain: "verify-1e415.firebaseapp.com",
+  projectId: "verify-1e415",
+  storageBucket: "verify-1e415.appspot.com",
+  messagingSenderId: "962388958405",
+  appId: "1:962388958405:web:6d763bf92fb86eddb4e4ac",
+  measurementId: "G-QJ7H680ZYB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
