8c7efee8cf89af77e8e1355a3b4e8d2c0d87131e
sumit <sumitsaini576@gmail.com>
2023-11-09 18:33:02 +0530

    .

--- eduor/src/pages/VerifyEmail.jsx
+++ eduor/src/pages/VerifyEmail.jsx
@@ -1,65 +1,57 @@
 import React from "react";
-import { Formik, Form } from "formik";
 
 const VerifyEmail = () => {
-
-
   let link = window.location.href;
-  let nlink=link.replace("localhost:3000","localhost:4000");
-  console.log("new",nlink);
+  let nlink = link.replace("localhost:3000", "localhost:4000");
+  console.log("new", nlink);
+
+  const onSubmit = () => {
+    console.log("hii");
+    // console.log("details", email,password);
+    fetch(nlink, {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({}),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "userLogin");
+        if (data.status === "OK") {
+          alert("Verification successful");
+          window.location.href = "/";
+        } 
+        else {
+          if (data.status.code === 11000) {
+            alert("Email already verified, Please login");
+            window.location.href = "/";
+            console.log(data.status);
+          }
+        else {
+            alert(data.data.message);
+          }
+        }
+      });
+  };
+
   return (
     <div className="flex justify-center items-center w-full py-10">
-      <Formik
-        initialValues={{
-          
-        }}
-        validate={(values) => {
-          const errors = {};
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            // console.log("details", email,password);
-            fetch(nlink, {
-              method: "POST",
-              crossDomain: true,
-              headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-                "Access-Control-Allow-Origin": "*",
-              },
-              body: JSON.stringify({
-              }),
-            })
-              .then((res) => res.json())
-              .then((data) => {
-                if(data.status==="OK"){
-                    alert("verification successful");
-                    window.location.href='/';
-                }
-                else{
-                  if(data.status.code === 11000){      
-                      alert("Email already verified, Please login")
-                      window.location.href='/';
-                      console.log(data.status);  
-                  }
-                  else{
-                    alert(data.data.message);
-                  }
-                }
-              });
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className="flex flex-col w-2/5  bg-bgcolor-600 rounded-md p-12 items-center">
-          <p className="text-center">Click on verify now and your account will be created</p>
-            <button className="btn my-5 px-10" type="submit" disabled={isSubmitting}>Verify Now & Login</button>
-            
-          </Form>
-        )}
-      </Formik>
+      <div className="flex flex-col items-center w-2/5  bg-bgcolor-600 rounded-md p-12">
+        <p className="head">Welcome To Eduor!</p>
+        <p className="para">Please verify your email</p>
+
+        <button
+          className="btn mt-5 px-10"
+          type="submit"
+          onClick={() => onSubmit()}
+        >
+          Verify and Login
+        </button>
+      </div>
     </div>
   );
 };
